f7ccfe3dac7909d98d645ff1e6fae4914d57523c
Armando Muceku <armandomucekuu@gmail.com>
2021-01-10 18:38:23 +0100

    refactor and project setup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,39 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { theme, colors } from '@/../src/theme.ts'
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.config.productionTip = false;
+Vue.mixin({
+  computed: {
+    $colors() {
+      return colors
+    },
+    $theme() {
+      return theme
+    },
+  },
+})
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.token.accessToken) {
+      next()
+    } else {
+      router.push({ name: 'login' })
+    }
+  } else {
+    next()
+  }
+})
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/theme.ts
@@ -0,0 +1,26 @@
+const theme: object = {
+  primary: 'black',
+  secondary: '#ff0000',
+  accent: '#8c9eff',
+  error: '#b71c1c',
+}
+
+const colors: object = {
+  primary: '#fef14b',
+  primaryDark: '#fddc1a',
+  primaryLight: '#fdf697',
+  secondary: '#ff0000',
+  secondaryDark: '#da0000',
+  secondaryLight: '#ff200c',
+  secondaryLighter: '#ff321e',
+  complementary: '#4b57fe',
+  analogousOne: '#fe994b',
+  analogousTwo: '#b0fe4b',
+  triadicOne: '#57fe4b',
+  triadicTwo: '#4bfef2',
+  primaryWhite: '#ffffff',
+  dark: '#000000',
+  darkLight: '#212121',
+}
+
+export { theme, colors }
